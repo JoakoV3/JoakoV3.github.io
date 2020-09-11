4fa186adb145a0e6f6c2538626eb7afa99ac8667
JoakoV3 <joaquinvarelaivan@gmail.com>
2020-09-11 17:22:33 -0300

    first commit

---
+++ precache-manifest.1f377f779754ece959f8867fe26092a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db226057278aee2d730ca5f927edf9a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dbc76fdb9ff8b51eafa3",
+    "url": "./static/css/2.b031c793.chunk.css"
+  },
+  {
+    "revision": "3efd40f6a9f866e1a6a9",
+    "url": "./static/css/main.534ad485.chunk.css"
+  },
+  {
+    "revision": "dbc76fdb9ff8b51eafa3",
+    "url": "./static/js/2.a2fa4387.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a2fa4387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3efd40f6a9f866e1a6a9",
+    "url": "./static/js/main.5659a8a5.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "2260eb1a0f48eb82d60fd7493339a9f7",
+    "url": "./static/media/Imagen3.2260eb1a.png"
+  },
+  {
+    "revision": "44388840b9868f3daed433b195eaf33e",
+    "url": "./static/media/Imagen5.44388840.png"
+  },
+  {
+    "revision": "9188df2a22509d52e5ee2b3db80d2f30",
+    "url": "./static/media/Imagen6.9188df2a.png"
+  },
+  {
+    "revision": "db7933e6a7f7acaf99a33f39b286974b",
+    "url": "./static/media/Javascript.db7933e6.png"
+  },
+  {
+    "revision": "a5b3879f4b6c62bd4edbf569e2a5019b",
+    "url": "./static/media/hero-image.a5b3879f.jpg"
+  },
+  {
+    "revision": "d3d76541eb4dc6bd39a4e11f2f163609",
+    "url": "./static/media/imagen7.d3d76541.png"
+  }
+]);
